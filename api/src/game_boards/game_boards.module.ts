fb37c2447bafbabbd11f4ef2aba2849771bf177c
Allan Gelman <allangelman4@gmail.com>
2022-12-28 13:22:03 -0500

    importing gamerows module to gameboard module

--- api/src/game_boards/game_boards.module.ts
+++ api/src/game_boards/game_boards.module.ts
@@ -3,9 +3,10 @@ import { GameBoardsService } from './game_boards.service';
 import { GameBoardsResolver } from './game_boards.resolver';
 import { GameBoard } from './entities/game_board.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GameRowsModule } from 'src/game_rows/game_rows.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GameBoard])],
+  imports: [TypeOrmModule.forFeature([GameBoard]), GameRowsModule],
   providers: [GameBoardsResolver, GameBoardsService],
   exports: [GameBoardsService],
 })
